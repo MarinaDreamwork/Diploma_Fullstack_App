a5c76d5c5056a32e9ccfc6f4d7601c2b270f3273
Marina Trushina <silaevamo@gmail.com>
2022-07-30 23:15:52 +0300

    changed path

--- app.js
+++ app.js
@@ -16,10 +16,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use('/api', routes);
 
+console.log('dirname', __dirname);
+
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.resolve(__dirname, 'client', 'build')));
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
